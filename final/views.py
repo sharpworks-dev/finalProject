c8b2e972d1e3dcf82af760de7eeadf89a6dce4a0
Ryan Sharp <rsharpworks@gmail.com>
2020-11-26 00:54:00 -0500

    forgot to make commits along the way but I did a lot of stuff
    Added views.py methods, urls.py paths, and html files to be able to view threads, posts, and comments

--- final/views.py
+++ final/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render, redirect
+from .models import *
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'final/test.html')
+    threads = Thread.objects.all()
+    context = {'threads': threads}
+    return render(request, 'final/test.html', context)
+
+
+def view_thread(request, id):
+    posts = Post.objects.filter(thread_id=id)
+    context = {'posts': posts}
+    return render(request, 'final/view-thread.html', context)
+
+def view_post(request, id):
+    post = Post.objects.get(id=id)
+    comments = Comment.objects.filter(post_Id=id)
+    context = {'post': post, 'comments': comments}
+    return render(request, 'final/view-post.html', context)
