6b9cfefbcd7acec2ccf522e897e8cd52629d9210
ChaseThompson8 <74880503+ChaseThompson8@users.noreply.github.com>
2020-12-09 03:42:35 -0500

    Python Anywhere setup

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -116,4 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ['static']
+# STATICFILES_DIRS = ['static']
+STATIC_ROOT = BASE_DIR / 'static'
